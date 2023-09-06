f5f1e6fefff45d58c70793d9aaa2f2979e6b34ef
lxhcaicai <2778763221@qq.com>
2023-09-06 21:48:01 +0800

    UTS Namespace demo

---
+++ namespace/uts/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// 注: 运行时需要 root 权限。
+func main() {
+	cmd := exec.Command("bash")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
